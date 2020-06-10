533e1198bd03ef2bd1371bcdfa3055dee8064fca
Luan Coleto <luancoleto@outlook.com>
2020-06-10 17:13:50 -0300

    Create routes

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const routes = express.Router();
+
+routes.get('/spoilers', () => {});
+
+routes.post('/spoilers', () => {});
+
+routes.put('/spoilers/:id', () => {});
+
+routes.delete('/spoiler/:id', () => {});
+
+
+export default routes;
