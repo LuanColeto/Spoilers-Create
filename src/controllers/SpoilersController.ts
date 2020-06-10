5dba8fc48fa8b4382005b29bffde7d746d4efb53
Luan Coleto <luancoleto@outlook.com>
2020-06-10 17:49:13 -0300

    Update routes

--- src/controllers/SpoilersController.ts
+++ src/controllers/SpoilersController.ts
@@ -2,7 +2,21 @@ import {Request, Response} from 'express';
 import knex from '../database/connection';
 
 class SpoilersController {
+    async index(request: Request, response: Response) {
 
+    }
+
+    async create(request: Request, response:Response) {
+
+    }
+
+    async update(request: Request, response: Response) {
+
+    }
+
+    async dedtroy(request: Request, response: Response) {
+
+    }
 }
 
 export default SpoilersController;
--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,13 @@ import SpoilersController from './controllers/SpoilersController';
 const routes = express.Router();
 const spoilersController = new SpoilersController();
 
-routes.get('/spoilers', );
+routes.get('/spoilers', spoilersController.index);
 
-routes.post('/spoilers', () => {});
+routes.post('/spoilers', spoilersController.create);
 
-routes.put('/spoilers/:id', () => {});
+routes.put('/spoilers/:id', spoilersController.update);
 
-routes.delete('/spoiler/:id', () => {});
+routes.delete('/spoiler/:id', spoilersController.dedtroy);
 
 
 export default routes;
