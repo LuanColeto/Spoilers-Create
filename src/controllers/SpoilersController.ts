df271b7bcf027133e3c76ecb52bb5c5fc3a90ce2
Luan Coleto <luancoleto@outlook.com>
2020-06-11 00:53:13 -0300

    Create create controller

--- src/controllers/SpoilersController.ts
+++ src/controllers/SpoilersController.ts
@@ -9,6 +9,30 @@ class SpoilersController {
     }
 
     async create(request: Request, response:Response) {
+        const {
+            title,
+            name,
+            description
+        } = request.body;
+
+        const trx = await knex.transaction();
+
+        const spoiler = {
+            title,
+            name,
+            description
+        };
+
+        const insertedIds = await trx('spoilers').insert(spoiler);
+
+        const spoiler_id = insertedIds[0];
+
+        await trx.commit();
+
+        response.json({
+            id: spoiler_id,
+            ...spoiler
+        })
 
     }
 
