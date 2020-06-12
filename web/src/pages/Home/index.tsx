caa2b14e1f2523ed6d33b1d675f61b3f00ef88e6
Luan Coleto <luancoleto@outlook.com>
2020-06-12 11:12:28 -0300

    Start the home page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div>
+            <nav className="navbar navbar-expand-md navbar-dark mb-4">
+                <h1 className=" text-dark" style={{marginBottom: '40px', marginTop: '40px'}}>#Spoilers</h1>
+            </nav>
+            <main role="main" className="container">
+                <div className="jumbotron">
+                    <h1 className="text-center" style={{marginBottom: '40px'}}>Cuidado!</h1>
+                    <p className="lead text-center">Você está entrando em um zona de spoilers</p>
+                    <p className="lead text-center">Estou ciente e quero continuar</p>
+                    <a className="btn btn-lg btn-primary " style={{backgroundColor: '#a64452'}} href="/docs/4.5/components/navbar/" role="button">Continuar</a>
+                </div>
+            </main>
+        </div>
     )
 };
 
