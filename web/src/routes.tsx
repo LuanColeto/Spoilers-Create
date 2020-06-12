60730eaf27ee98a8762d316ddbee390054edf8cb
Luan Coleto <luancoleto@outlook.com>
2020-06-12 00:31:43 -0300

    Create Routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { Route, BrowserRouter} from 'react-router-dom';
 
 //Pages
+import Home from './pages/Home';
+import Spoilers from './pages/Spoilers';
+import CreateSpoilers from './pages/CreateSpoiler';
+import DeleteSpoilers from './pages/DeleteSpoiler';
+import UpdateSpoilers from './pages/UpdateSpoiler';
 
 const Routes = () => {
   return (
-      <div></div>
+      <BrowserRouter>
+          <Route component={Home} path="/" exact />
+          <Route component={Spoilers} path="/spoilers" />
+          <Route component={CreateSpoilers} path="/create" />
+          <Route component={DeleteSpoilers} path="/delete" />
+          <Route component={UpdateSpoilers} path="/update" />
+      </BrowserRouter>
   )
 };
 
