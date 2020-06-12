69fe51782fc1e9936f211326eebaf5a65faa9b6f
Luan Coleto <luancoleto@outlook.com>
2020-06-12 10:42:19 -0300

    Create Header

--- web/src/components/cabecalho.jsx
+++ web/src/components/cabecalho.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Cabecalho = () => {
     return (
-        <div >
-
-        </div>
+        <header>
+            <div className="jumbotron">
+                <div className="container">
+                    <h1 className="display-3">#Spoilers</h1>
+                    <p style={{marginTop: '40px'}}>Faça Spoilers, não faça guerra :)</p>
+                    <p><a className="btn btn-primary btn-lg" style={{backgroundColor: '#34CB79', border: 'none', marginTop: '40px'}} href="#" role="button">Criar Spoiler</a></p>
+                </div>
+            </div>
+        </header>
     )
-}
+};
 
 export default Cabecalho;
