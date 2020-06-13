362ade963e47203669b9a5278098bdfd768497d4
Luan Coleto <luancoleto@outlook.com>
2020-06-12 22:04:48 -0300

    Update Header

--- web/src/components/cabecalho.jsx
+++ web/src/components/cabecalho.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Cabecalho = () => {
     return (
@@ -7,7 +8,7 @@ const Cabecalho = () => {
                 <div className="container">
                     <h1 className="display-3">#Spoilers</h1>
                     <p style={{marginTop: '40px'}}>Faça Spoilers, não faça guerra :)</p>
-                    <p><a className="btn btn-primary btn-lg" style={{backgroundColor: '#34CB79', border: 'none', marginTop: '40px'}} href="#" role="button">Criar Spoiler</a></p>
+                    <Link to="/create"><a className="btn btn-primary btn-lg" style={{backgroundColor: '#34CB79', border: 'none', marginTop: '40px'}} href="" role="button">Criar Spoiler</a></Link>
                 </div>
             </div>
         </header>
