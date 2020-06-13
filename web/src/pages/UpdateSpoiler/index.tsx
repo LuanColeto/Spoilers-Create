2ef49e3cb1a092d4c6d3aed4f610ae9a2e71a762
Luan Coleto <luancoleto@outlook.com>
2020-06-13 19:08:38 -0300

    Update UpdateSpoiler

--- web/src/pages/UpdateSpoiler/index.tsx
+++ web/src/pages/UpdateSpoiler/index.tsx
@@ -5,8 +5,46 @@ const UpdateSpoiler = () => {
     return (
         <div>
             <Cabecalho/>
-            <form>
-                
+            <form className="container">
+                <fieldset>
+                    <legend>Criar Spoiler</legend>
+                    <div className="form-group">
+                        <label htmlFor="espoliador">Espoliador</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="espoliador"
+                            name="espoliador"
+                            placeholder="Fulano de Tal"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="titulo">Título</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="titulo"
+                            name="titulo"
+                            aria-describedby="tituloAjuda"
+                            placeholder="Star Wars"
+                        />
+                        <small id="tituloAjuda" className="form-text text-muted">
+                            Um título pode ser um filme, série, livro...
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="descricao">Descrição</label>
+                        <textarea
+                            className="form-control"
+                            id="descricao"
+                            name="descricao"
+                            placeholder="Luke morre"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Enviar
+                    </button>
+                </fieldset>
             </form>
         </div>
     )
