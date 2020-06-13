380399045e72205d5cffa36be609ac6b462429df
Luan Coleto <luancoleto@outlook.com>
2020-06-12 22:04:32 -0300

    Create Spoiler Card

--- web/src/pages/Spoilers/index.tsx
+++ web/src/pages/Spoilers/index.tsx
@@ -1,57 +1,29 @@
 import React from 'react';
-import Cabecalho from '../../components/cabecalho'
+import Cabecalho from '../../components/cabecalho';
+import { Link } from 'react-router-dom'
 
 const Spoilers = () => {
     return (
         <div>
             <Cabecalho/>
             <div className="container">
-                <div className="card-deck mb-3 text-center">
-                    <div className="card mb-4 shadow-sm" >
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Free</h4>
-                        </div>
+                <div className="row">
+                    <div className="card mb-4">
+                        <h5 className="card-header">Luan</h5>
                         <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$0 <small className="text-muted">/ mo</small>
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="card mb-4 shadow-sm">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Pro</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$15 <small className="text-muted">/ mo</small>
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>20 users included</li>
-                                <li>10 GB of storage</li>
-                                <li>Priority email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-primary">Get started</button>
-                        </div>
-                    </div>
-                    <div className="card mb-4 shadow-sm">
-                        <div className="card-header">
-                            <h4 className="my-0 font-weight-normal">Enterprise</h4>
-                        </div>
-                        <div className="card-body">
-                            <h1 className="card-title pricing-card-title">$29 <small className="text-muted">/ mo</small>
-                            </h1>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>30 users included</li>
-                                <li>15 GB of storage</li>
-                                <li>Phone and email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                            <button type="button" className="btn btn-lg btn-block btn-primary">Contact us</button>
+                            <div className="media">
+                                <img className="mr-3" alt="Spoiler"  />
+                                <div className="media-body">
+                                    <h5>Star Wars</h5>
+                                    <p>Darth Vader é pai do Luke</p>
+                                </div>
+                            </div>
+                            <Link to="/delete" className="btn btn-danger mr-3" role="button">
+                                Remover
+                            </Link>
+                            <Link to="/update" className="btn btn-primary" role="button">
+                                Editar
+                            </Link>
                         </div>
                     </div>
                 </div>
