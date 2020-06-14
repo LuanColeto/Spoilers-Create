5e09bd6a977941ec8bb36d10632758ee38d10fd7
Luan Coleto <luancoleto@outlook.com>
2020-06-13 23:21:20 -0300

    Update Delete card

--- web/src/pages/DeleteSpoiler/index.tsx
+++ web/src/pages/DeleteSpoiler/index.tsx
@@ -8,18 +8,20 @@ const DeleteSpoiler = () => {
         <div>
             <Cabecalho/>
             <div className="container">
-                <div className="row">
-                    <div className="card mb-4">
-                        <h5 className="card-header">Star Wars</h5>
-                        <div className= "card-body">
-                            <p style={{marginBottom: '50px'}}>Tem certeza que deseja remover esse <br/>Spoiler? As pessoas podem sentir falta <br/>dele :/</p>
-                            <p className="text-center">Darth Vader é pai do Luke</p>
-                            <p className="text-center" style={{color: '#6c757d'}}>- Luan</p>
-                            <Link to="/delete" className="btn btn-danger mr-3 col" role="button">
-                                Remover
-                            </Link>
+                <div className="row justify-content-md-center">
+                    <main className="col-xl-4 col-lg-4">
+                        <div className="card mb-4">
+                            <h5 className="card-header">Star Wars</h5>
+                            <div className= "card-body">
+                                <p style={{marginBottom: '50px'}}>Tem certeza que deseja remover esse <br/>Spoiler? As pessoas podem sentir falta <br/>dele :/</p>
+                                <p className="text-center">Darth Vader é pai do Luke</p>
+                                <p className="text-center" style={{color: '#6c757d'}}>- Luan</p>
+                                <Link to="/delete" className="btn btn-danger mr-3 col" role="button">
+                                    Remover
+                                </Link>
+                            </div>
                         </div>
-                    </div>
+                    </main>
                 </div>
             </div>
         </div>
