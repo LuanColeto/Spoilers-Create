7c4c39cfd38b172ee03cced97362bbf32d03504f
Luan Coleto <luancoleto@outlook.com>
2020-06-13 23:17:12 -0300

    Update Spoiler card

--- web/src/pages/Spoilers/index.tsx
+++ web/src/pages/Spoilers/index.tsx
@@ -7,25 +7,27 @@ const Spoilers = () => {
         <div>
             <Cabecalho/>
             <div className="container">
-                <div className="row">
-                    <div className="card mb-4">
-                        <h5 className="card-header">Luan</h5>
-                        <div className="card-body">
-                            <div className="media">
-                                <img className="mr-3" alt="Spoiler"  />
-                                <div className="media-body">
-                                    <h5>Star Wars</h5>
-                                    <p>Darth Vader é pai do Luke</p>
+                <div className="row justify-content-md-center">
+                    <main className="col-xl-4 col-lg-4">
+                        <div className="card mb-4">
+                            <h5 className="card-header">Luan</h5>
+                            <div className="card-body">
+                                <div className="media">
+                                    <img className="mr-3" alt="Spoiler"  />
+                                    <div className="media-body">
+                                        <h5>Star Wars</h5>
+                                        <p>Darth Vader é pai do Luke</p>
+                                    </div>
                                 </div>
+                                <Link to="/delete" className="btn btn-danger mr-3" role="button">
+                                    Remover
+                                </Link>
+                                <Link to="/update" className="btn btn-primary" role="button">
+                                    Editar
+                                </Link>
                             </div>
-                            <Link to="/delete" className="btn btn-danger mr-3" role="button">
-                                Remover
-                            </Link>
-                            <Link to="/update" className="btn btn-primary" role="button">
-                                Editar
-                            </Link>
                         </div>
-                    </div>
+                    </main>
                 </div>
             </div>
         </div>
