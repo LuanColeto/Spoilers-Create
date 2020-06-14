f8536ee5d3b9c77e29dc6a2efd08c708eb14cb71
Luan Coleto <luancoleto@outlook.com>
2020-06-13 22:36:11 -0300

    Delete page

--- web/src/pages/DeleteSpoiler/index.tsx
+++ web/src/pages/DeleteSpoiler/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import Cabecalho from '../../components/cabecalho'
+import Cabecalho from '../../components/cabecalho';
+import { Link } from 'react-router-dom';
 
 
 const DeleteSpoiler = () => {
     return (
         <div>
             <Cabecalho/>
-            <h1>DELETAR SPOILER</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="card mb-4">
+                        <h5 className="card-header">Star Wars</h5>
+                        <div className= "card-body">
+                            <p style={{marginBottom: '50px'}}>Tem certeza que deseja remover esse <br/>Spoiler? As pessoas podem sentir falta <br/>dele :/</p>
+                            <p className="text-center">Darth Vader é pai do Luke</p>
+                            <p className="text-center" style={{color: '#6c757d'}}>- Luan</p>
+                            <Link to="/delete" className="btn btn-danger mr-3 col" role="button">
+                                Remover
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
